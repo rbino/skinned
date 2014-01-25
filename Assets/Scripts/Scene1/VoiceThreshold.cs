86793d6b212b3e3146bc124b29ad87050b0f501a
Riccardo Binetti <punkerbino@gmail.com>
2014-01-25 21:05:36 +0100

    Stabilized trigger input

--- Assets/Scripts/Scene1/VoiceThreshold.cs
+++ Assets/Scripts/Scene1/VoiceThreshold.cs
@@ -5,31 +5,33 @@ public class VoiceThreshold : MonoBehaviour {
 
 	public GUIText counterText;
 	private int counter=0;
+	private bool backHome=true;
+	private Collider2D square;
 
 	// Use this for initialization
 	void Start () {
 //		timer.enabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (square){
+			if (square.transform.position.y < -0.8){
+				backHome = true;
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-
-	//	StartCoroutine (thresholdTimer());
+		square = other;
+		if (backHome){
 		counterText.enabled = true;
 		if (++counter >= 3){
 			counter = 0;
 			transform.position = new Vector3 (transform.position.x, transform.position.y + 0.4f, transform.position.z);
 		}
 		counterText.text = "" + counter;
-	}
-
-	void OnTriggerExit2D(Collider2D other){
-	//	timer.enabled = false;
-	//	timer.text = "-1";
+		}
 	}
 
 	IEnumerator thresholdTimer(){
