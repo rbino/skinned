2eab75ef8ee6c12eb5f78fe673d270813b27f376
Riccardo Binetti <punkerbino@gmail.com>
2014-01-25 21:07:19 +0100

    Removed useless function

--- Assets/Scripts/Scene1/VoiceThreshold.cs
+++ Assets/Scripts/Scene1/VoiceThreshold.cs
@@ -33,16 +33,4 @@ public class VoiceThreshold : MonoBehaviour {
 		counterText.text = "" + counter;
 		}
 	}
-
-	IEnumerator thresholdTimer(){
-		while (int.Parse(counterText.text)>0) {
-			counterText.text = (int.Parse(counterText.text) - 1).ToString();
-			yield return new WaitForSeconds(1.0f);
-		}
-		counterText.enabled = false;
-		if (int.Parse (counterText.text) == 0) {
-			transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
-		}
-		
-	}
 }
