6c2a93ae3cc86658d8054548dc1ad0ee3153e360
EugenioGuidetti <eugenioguidetti@yahoo.it>
2014-01-25 14:29:00 +0100

    New empty objects: mouth and deep throat.

--- Assets/Scripts/Scene2/Fingers.cs
+++ Assets/Scripts/Scene2/Fingers.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class Fingers : MonoBehaviour {
 
+	private float speedModule = 15f;
+	private Vector3 speedDirection = new Vector3(-1, 0, 0);
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start () {}
 	
 	// Update is called once per frame
 	void Update () {
