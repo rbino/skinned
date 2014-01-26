9d6992116c6609b3e72abb4d72bd8c27afe6a0dc
EugenioGuidetti <eugenioguidetti@yahoo.it>
2014-01-26 10:29:14 +0100

    Scene2 with 3 stage prepared.

--- Assets/Scripts/Scene2/Face.cs
+++ Assets/Scripts/Scene2/Face.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Face : MonoBehaviour {
 
+	public GameObject deepThroat;
+	private Vector3 stage2 = new Vector3(0.2f, -0.5f, 0);
+	private Vector3 stage3 = new Vector3(-0.1f, -0.5f, 0);
+
 	public Sprite inside;
 	private Sprite outside;
 	private bool insert = false;
@@ -23,5 +27,13 @@ public class Face : MonoBehaviour {
 			this.gameObject.GetComponent<SpriteRenderer>().sprite = outside;
 			insert = false;			
 		}
+	}
+
+	public void SetStage2 () {
+		deepThroat.transform.position = stage2;
+	}
+
+	public void SetStage3 () {
+		deepThroat.transform.position = stage3;
 	}
 }
