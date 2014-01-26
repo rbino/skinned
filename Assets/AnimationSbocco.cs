d6dec201f477d775e02da199246c8ef3f32de22b
DanieleGravina <daniele.gravina@mail.polimi.it>
2014-01-26 15:57:00 +0100

    Added sbocco animation and sound

---
+++ Assets/AnimationSbocco.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSbocco : MonoBehaviour {
+
+	public float minimum = 0;
+	public float maximum = 3;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x,
+		                                 1.81f - Mathf.Lerp(minimum, maximum, Time.time),
+		                                 transform.position.z);
+	}
+}
